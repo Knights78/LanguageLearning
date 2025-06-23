632457a65a961ffff836dbc7a283f745ea2c8d0d
Knights78 <saifushaikh102@gmail.com>
2025-06-23 20:25:15 +0530

    signup done

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,5 @@
+import { axiosInstance } from "./axios.js";
+export const signup = async (signupData) => {
+  const res = await axiosInstance.post('/auth/signup', signupData);
+  return res.data;
+}
